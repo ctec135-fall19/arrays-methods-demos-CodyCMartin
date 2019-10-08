e1acc836a2fa599e2b93dfd8abcb6c9ceaaf13b0
codycmartin <codycmartin89@gmail.com>
2019-10-08 15:18:07 -0700

    Complete!

---
+++ TuesdayDemo10_08/arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaring 
+            int[] myArray = new int[5];
+            //intitializing 
+            for(int i=0; i<myArray.Length; i++)
+            {
+                myArray[i] = i + 10;
+            }
+
+            // treversing
+            Console.WriteLine("print array elements");
+            foreach (int element in myArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine();
+        }
+    }
+}
